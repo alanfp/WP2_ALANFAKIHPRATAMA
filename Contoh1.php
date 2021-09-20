45b2d5f0db228f6c75e45d623261db8752d691ce
alanfp <91037291+alanfp@users.noreply.github.com>
2021-09-20 10:14:11 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Alan Fakih Pratama,
+        Saya tinggal di daerah Batutulis,
+        Saya lahir pada tanggal 19 Maret 2002.";
+    }
+}
