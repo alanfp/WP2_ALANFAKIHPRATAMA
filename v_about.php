85829e595624f0f8a401648cc6bb9184e38850c9
alanfp <91037291+alanfp@users.noreply.github.com>
2021-10-05 09:06:56 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,20 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Alan</li>
+ <li>Nama Belakang : Fakih</li>
+ </ul>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Lawanggintung No. 52A Batutulis,Bogor.</li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="none">
+ <li>Futsal</li>
+ </ul>
+</section>
